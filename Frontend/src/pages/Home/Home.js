f51d6966395d3f3c948c3538fa8340a11673edb8
Avi <sa3198154@gmail.com>
2024-06-08 20:20:49 +0530

    Home

--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
 import './Home.css'
 const Home = () => {
     return (
         <div>
             <Navbar />
             <div className='home-container'>
-                Generate Images on the go
-                <button className="learn-more">
-                    <span className="circle" aria-hidden="true">
-                        <span className="icon arrow"></span>
-                    </span>
-                    <span className="button-text">Learn More</span>
-                </button>
+                <div className='main'>
+                    <h1>Generate <br /> Images <br /> on the go</h1><div />
+                    <button className="learn-more">
+                        <span className="circle" aria-hidden="true">
+                            <span className="icon arrow"></span>
+                        </span>
+                        <Link to='/sign-up' className="button-text">Get Started</Link>
+                    </button>
+                </div>
             </div>
         </div>
     )
