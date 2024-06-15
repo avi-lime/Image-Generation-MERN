672f62deaef9809c2b3d30d061c4d2280b3ec63b
Avi <sa3198154@gmail.com>
2024-06-15 17:01:23 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,24 +11,24 @@ app.use(express.json())
 
 app.use('/api/v1/auth', authRouter)
 
-// app.use((req, res, next) => {
-//     let token = req.headers.authorization
-//     if (token && token.startsWith('Bearer')) {
-//         token = token.split(' ')[1]
+app.use((req, res, next) => {
+    let token = req.headers.authorization
+    if (token && token.startsWith('Bearer')) {
+        token = token.split(' ')[1]
 
-//         try {
-//             jwt.verify(token, process.env.SECRET)
-//             next()
-//         } catch (e) {
-//             console.log(e)
-//             return res.status(401).json({
-//                 message: 'Unauthorized ' + e.message
-//             })
-//         }
+        try {
+            jwt.verify(token, process.env.SECRET)
+            next()
+        } catch (e) {
+            console.log(e)
+            return res.status(401).json({
+                message: 'Unauthorized ' + e.message
+            })
+        }
 
-//     }
+    }
 
-// })
+})
 
 
 app.use('/api/v1/images', imageRouter)
